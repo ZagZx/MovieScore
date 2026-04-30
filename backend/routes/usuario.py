aa47fadbd3301004a258270b1e104cc36a009d71
ZagZx <azevedodvictor@gmail.com>
2026-04-30 01:32:12 -0300

    feat: patch e delete do usuario

--- backend/routes/usuario.py
+++ backend/routes/usuario.py
@@ -24,15 +24,54 @@ def buscar_usuario(id: int, session: SessionDep):
     
     return usuario
     
-@usuario_router.post("")
-def criar_usuario(usuario: UsuarioCreate, session: SessionDep):
+@usuario_router.post("", response_model=UsuarioRead)
+def criar_usuario(usuario_json: UsuarioCreate, session: SessionDep):
     novo_usuario = Usuario(
-        nome=usuario.nome,
-        email=usuario.email,
-        senha_hash=get_password_hash(usuario.senha)
+        nome=usuario_json.nome,
+        email=usuario_json.email,
+        senha_hash=get_password_hash(usuario_json.senha)
     )
     try:
         session.add(novo_usuario)
         session.commit()
     except:
         session.rollback()
+
+    return novo_usuario
+
+@usuario_router.patch("/{id}", response_model=UsuarioRead)
+def atualizar_usuario(id: int, usuario_json: UsuarioUpdate, session: SessionDep):
+    usuario = session.get(Usuario, id)
+    
+    if not usuario:
+        raise HTTPException(status_code=404)
+    
+    if usuario_json.nome:
+        usuario.nome = usuario_json.nome
+    if usuario_json.email:
+        usuario.email = usuario_json.email
+    if usuario_json.senha:
+        usuario.senha_hash = get_password_hash(usuario_json.senha)
+
+    try: 
+        session.commit()
+        session.refresh(usuario)
+    except:
+        session.rollback()
+
+    return usuario
+    
+@usuario_router.delete("/{id}")
+def deletar_usuario(id: int, session: SessionDep):
+    usuario = session.get(Usuario, id)
+
+    if not usuario:
+        raise HTTPException(status_code=404)
+   
+    session.delete(usuario)
+
+    try:
+        session.commit()
+    except:
+        session.rollback()
+
