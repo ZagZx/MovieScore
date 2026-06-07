adf6705ea9740f181d7963274a86f5d8102b563b
ZagZx <azevedodvictor@gmail.com>
2026-06-07 12:59:39 -0300

    fix: import do router de séries no __init__ e gerar_env removido do main

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from routes import animes_router, filmes_router, usuario_router, series_router
-from utils import gerar_env
 from database import create_db
 
-
-gerar_env()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,3 +1,4 @@
 from .anime import animes_router
+from .serie import series_router
 from .filme import filmes_router
 from .usuario import usuario_router
