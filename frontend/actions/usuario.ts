cd1ee54aa543c8829fa0a986b0da8c3b0ae57557
ZagZx <azevedodvictor@gmail.com>
2026-08-24 13:10:43 -0300

    refactor: correção de erro no build e pasta routes movida para lib

--- frontend/actions/usuario.ts
+++ frontend/actions/usuario.ts
@@ -1,12 +1,12 @@
 "use server";
 
-import { GET_USUARIOS_URL, POST_USUARIO_URL } from "@/routes/usuario";
+import { GET_USUARIOS_URL, POST_USUARIO_URL } from "@/lib/routes/usuario";
 import { CreateUsuarioBody, CreateUsuarioResponse, GetUsuariosBody, GetUsuariosResponse } from "@/lib/types/usuario";
 import axios from "axios";
 
 export async function getUsuarios(data: GetUsuariosBody){
     try {
-        const response = await axios.get<GetUsuariosResponse | any>(GET_USUARIOS_URL(), {
+        const response = await axios.get<GetUsuariosResponse>(GET_USUARIOS_URL(), {
             params: {
                 limit: data.limit,
                 cursor: data.cursor
@@ -23,7 +23,7 @@ export async function getUsuarios(data: GetUsuariosBody){
 
 export async function postCreateUsuario(data: CreateUsuarioBody){
     try {
-        const response = await axios.post<CreateUsuarioResponse | any>(POST_USUARIO_URL(), 
+        const response = await axios.post<CreateUsuarioResponse>(POST_USUARIO_URL(), 
             {
                 nome: data.nome,
                 email: data.email,
