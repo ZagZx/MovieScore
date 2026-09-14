1ab26fc54d744469075a0af16cfff327d328fa98
alanpereiradasilva <pereira.alan@escolar.ifrn.edu.br>
2026-09-14 08:01:53 -0300

    feat: criação das rotas buscar-filmes-id e listar-filmes-populares

--- backend/routes/filme.py
+++ backend/routes/filme.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 
 from constants import TMDB_API_URL, HEADERS_TMDB, PARAMS_TMDB
 from utils import get_data, ExternalAPIException
@@ -28,3 +28,32 @@ def listar_filmes_em_alta():
         return get_data(url, PARAMS_TMDB, HEADERS_TMDB)
     except ExternalAPIException as exc:
         raise HTTPException(status_code=exc.status_code, detail=exc.message)
+
+
+@filmes_router.get("/populares")
+def listar_filmes_populares():
+    url = TMDB_API_URL + "/movie/popular"
+
+    try:
+        return get_data(url, PARAMS_TMDB, HEADERS_TMDB)
+    except ExternalAPIException as exc:
+        raise HTTPException(status_code=exc.status_code, detail=exc.message)
+
+
+@filmes_router.get("/{filme_id}")
+def buscar_filme_id(filme_id: int):
+    url = TMDB_API_URL + f"/movie/{filme_id}"
+
+    try:
+        data = get_data(url, PARAMS_TMDB, HEADERS_TMDB)
+    except ExternalAPIException as exc:
+        raise HTTPException(status_code=exc.status_code, detail=exc.message)
+
+    if data.get("success") is False:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Filme não encontrado.",
+        )
+
+    return data
+
